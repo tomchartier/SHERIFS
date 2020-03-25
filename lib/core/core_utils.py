62a540b3038d6c84d5374bcbaaba9d9360aa5265
Thomas Chartier <tchartier@Thomass-MacBook-Pro.local>
2020-03-25 18:11:36 +0100

    Update core: fault budget managment

---
+++ lib/core/core_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""SHERIFS
+Seismic Hazard and Earthquake Rates In Fault Systems
+
+Version 1.2
+
+@author: Thomas Chartier
+"""
+import numpy as np
+  
+import collections, functools, operator
+  
