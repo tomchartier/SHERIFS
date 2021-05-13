09d700d70f75aeb6dec89b0349b9db708f85b897
tomchartier <thomaschartier0@gmail.com>
2021-05-13 09:49:00 +0200

    move precomp to toml

--- 0_build_ruptures.py
+++ 0_build_ruptures.py
@@ -25,67 +25,80 @@ from precomp_utils import *
 from build_scenarios import *
 from build_sections import *
 from jumps import *
+import toml
 
 # If you are running SHERIFS with spyder define "input_file" here. Then run.
 
 def build_rup(input_file):
     debut = time.time()
 
-    
+
     print ('\nPreparting the rutpures for SHERIFS\n')
 
     # Load the input file
-    lines = open(input_file,'r').readlines()
-    lines = [line.rstrip('\n') for line in lines]
-    apply_sr_reduction = 0.
-    for line in lines:
-        if "Run_Name" in line :
-            Run_Name = line.split(':')[1].replace(' ','')
-        if "File_Oiler" in line :
-            File_Oiler = line.split(':')[1].replace(' ','')
-        if "File_out" in line :
-            File_out = line.split(':')[1].replace(' ','')
-        if "File_Mmax_areas" in line :
-            File_Mmax_areas = line.split(':')[1].replace(' ','')
-        if "Model_name" in line :
-            Model_name = line.split(':')[1].replace(' ','')
-        if "rupture_mesh_spacing" in line :
-            rupture_mesh_spacing = float(line.split(':')[1].replace(' ',''))
-            
+    param = toml.load(input_file)
 
-                    
-        if "File_Mu" in line :
-            f_mu = line.split(':')[1].replace(' ','')
-            
-        if "jump_dist" in line :
-            jump_dist = float(line.split(':')[1].replace(' ',''))
-        if "apply_sr_reduction" in line :
-            apply_sr_reduction = float(line.split(':')[1].replace(' ',''))
-            
+    # lines = open(input_file,'r').readlines()
+    # lines = [line.rstrip('\n') for line in lines]
+    # apply_sr_reduction = 0.
+    # for line in lines:
+    #     if "Run_Name" in line :
+    #         Run_Name = line.split(':')[1].replace(' ','')
+    #     if "File_Oiler" in line :
+    #         File_Oiler = line.split(':')[1].replace(' ','')
+    #     if "File_out" in line :
+    #         File_out = line.split(':')[1].replace(' ','')
+    #     if "File_Mmax_areas" in line :
+    #         File_Mmax_areas = line.split(':')[1].replace(' ','')
+    #     if "Model_name" in line :
+    #         Model_name = line.split(':')[1].replace(' ','')
+    #     if "rupture_mesh_spacing" in line :
+    #         rupture_mesh_spacing = float(line.split(':')[1].replace(' ',''))
+    #
+    #
+    #
+    #     if "File_Mu" in line :
+    #         f_mu = line.split(':')[1].replace(' ','')
+    #
+    #     if "jump_dist" in line :
+    #         jump_dist = float(line.split(':')[1].replace(' ',''))
+    #     if "apply_sr_reduction" in line :
+    #         apply_sr_reduction = float(line.split(':')[1].replace(' ',''))
+    
+    Run_Name = param["Run_Name"]
+    File_Oiler = param["pre"]["File_Oiler"]
+    File_out = param["pre"]["File_out"]
+    File_Mmax_areas = param["pre"]["File_Mmax_areas"]
+    Model_name = param["pre"]["Model_name"]
+    rupture_mesh_spacing = param["pre"]["rupture_mesh_spacing"]
+    f_mu = param["pre"]["File_Mu"]
+    jump_dist = param["pre"]["jump_dist"]
+    apply_sr_reduction = param["pre"]["apply_sr_reduction"]
+
     path = "input/"+Run_Name
-            
+
     # Reading things
     faults,nb_faults = read_oiler_file(File_Oiler)
-    
+
     #bounding box for faults
     maxmin_pt_lon, maxmin_pt_lat = find_bounding_box(faults)
-    
+
     #find possible fault asso
     assso_fault = find_possible_asso(maxmin_pt_lon,maxmin_pt_lat)
-    
+
     # calc fault dimensions
     f_lengths, f_areas = calc_f_dims(faults,)
-    
+
     # cutting into smaller sections
     f_for_sherifs,id_sections_fault,sections_areas_tot,sections_lengths_tot = cut_faults(faults,
     f_lengths,
     f_areas,
     path,
     rupture_mesh_spacing)
-    
+
     # force jumps
     force_jump_on_fault = force_jump_list()
-    
+
     # find jumps
     section_jump = find_jumps(f_for_sherifs,
     assso_fault,
@@ -93,23 +106,23 @@ def build_rup(input_file):
     jump_dist,
     path,
     force_jump_on_fault)
-    
+
     # export section points
     export_sections_pts(f_for_sherifs,path)
-    
+
     # find max rupture size for each section
     f_for_sherifs = find_sections_Mmax(f_for_sherifs,File_Mmax_areas)
-    
+
     # create SHERIFS input dict for fault prperties
     f_for_sherifs = to_sherifs(f_for_sherifs,
     faults,
     Model_name,
     apply_sr_reduction,
     f_mu)
-    
+
     # create binning in mag
     binning_in_mag, nb_rup_per_bin = mag_bin_distr(f_for_sherifs)
-    
+
     # CORE : create the list of ruptures
     rup = build_scenarios(f_for_sherifs,
     id_sections_fault,
@@ -118,13 +131,13 @@ def build_rup(input_file):
     binning_in_mag,
     nb_rup_per_bin,
     section_jump)
-    
+
     # write SHERIFS input file for faults
     write_section_json(f_for_sherifs,File_out)
-    
+
     # write SHERIFS input file for ruptures
     write_rupt_file(rup,Run_Name)
-    
+
     # Create visualization of ruptures
     #visu_rup(f_for_sherifs,rup,rups_length,rups_mag,path)
 
